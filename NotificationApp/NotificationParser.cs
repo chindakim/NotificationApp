9aed479d724c870082d7a419b9eb4eb9815d99c5
chindakim <kimchinda.it@gmail.com>
2023-11-17 12:25:51 +0700

    modified channels

--- NotificationApp/NotificationParser.cs
+++ NotificationApp/NotificationParser.cs
@@ -2,8 +2,12 @@ namespace NotificationApp;
 
 public class NotificationParser
 {
-    private readonly List<string> _channels = new List<string> { "BE", "FE", "QA", "Urgent" };
+    private readonly List<string> _channels;
 
+    public NotificationParser(List<string> channels)
+    {
+        this._channels = channels;
+    }
     public List<string> ParseNotificationChannels(string msg)
     {
         var tags = msg.Split('[', ']').Where((_, i) => i % 2 != 0).ToList();
